65857f2edbdbc78367ee8fd909647ab97813b6f1
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-02-10 16:03:04 -0300

    organizando arquivos do header

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,19 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import { NavigationUl } from "./NavigationStyles";
 
 import NavigationItem from "./NavigationItem/NavigationItem";
-
-const NavigationUl = styled.ul`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background-color: black;
-  opacity: 0.85;
-  justify-content: space-between;
-  list-style: none;
-  width: 100%;
-  height: 6vh;
-`;
 
 const NavigationItems = () => {
   return (
---
+++ src/components/Navigation/NavigationItems/NavigationStyles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const NavigationUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: black;
+  opacity: 0.85;
+  justify-content: space-between;
+  list-style: none;
+  width: 100%;
+  height: 6vh;
+`;
+
+export const StyledLink = styled(NavLink)`
+  color: grey;
+  background-color: transparent;
+  display: flex;
+  width: 100px;
+  height: 6vh;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border: 1px solid black;
+  text-decoration: none;
+  font-weight: bolder;
+
+  :hover {
+    color: white;
+    background-color: grey;
+    transition: 0.5s;
+  }
+
+  &.${(props) => props.activeClassName} {
+    color: red;
+  }
+`;
+
+//<StyledLink activeClassName="any" />;
+
+export const WrappNavLink = styled.div`
+  background-color: gree;
+  width: 100px;
+  height: 100%;
+`;
